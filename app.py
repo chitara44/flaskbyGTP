9ba7b44341dcfeae02957c908925c1f4a981ab92
Ricardo Andres Naranjo Loaiza <chitara44@gmail.com>
2023-06-21 23:51:31 -0500

    adjusted endpoint

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/drafts', methods=['GET'])
 def get_drafts():
     # Reads a CSV file to get all the information within
-    with open('expdata2.csv', newline='') as csvfile:
+    with open('expdata.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         users = []
         for row in reader:
@@ -22,7 +22,7 @@ def create_draft():
     # get the customers data
     data = request.get_json()
     # Add a new draft to CSV file
-    with open('expdata2.csv', 'a', newline='') as csvfile:
+    with open('expdata.csv', 'a', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=['idSorteo', 'fecha', 'tipo', 'ganador', 'nuevo', 'n1', 'n2', 'n3', 'n4', 'n5', 'sb'])
         writer.writerow(data)
     # retrieves new draft data as a JSON object
@@ -35,14 +35,14 @@ def update_draft(idSorteo):
     # get the customers data
     data = request.get_json()
     # Update a draft to CSV file
-    with open('expdata2.csv', 'r', newline='') as csvfile:
+    with open('expdata.csv', 'r', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         sorteos = []
         for row in reader:
             if int(row['idSorteo']) == idSorteo:
                 row.update(data)
             sorteos.append(row)
-    with open('expdata2.csv', 'w', newline='') as csvfile:
+    with open('expdata.csv', 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=['idSorteo', 'fecha', 'tipo', 'ganador', 'nuevo', 'n1', 'n2', 'n3', 'n4', 'n5', 'sb'])
         writer.writeheader()
         writer.writerows(sorteos)
@@ -53,13 +53,13 @@ def update_draft(idSorteo):
 @app.route('/drafts/<int:idSorteo>', methods=['DELETE'])
 def delete_draft(idSorteo):
     # Deletes a draft from CSV file
-    with open('expdata2.csv', 'r', newline='') as csvfile:
+    with open('expdata.csv', 'r', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         users = []
         for row in reader:
             if int(row['idSorteo']) != idSorteo:
                 users.append(row)
-    with open('expdata2.csv', 'w', newline='') as csvfile:
+    with open('expdata.csv', 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=['idSorteo', 'fecha', 'tipo', 'ganador', 'nuevo', 'n1', 'n2', 'n3', 'n4', 'n5', 'sb'])
         writer.writeheader()
         writer.writerows(users)
